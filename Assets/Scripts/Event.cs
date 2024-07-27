9f8e29ac4b6401178e09745822420fa411b7e92c
Guilherme Cesário <guilhermecesario1808@gmail.com>
2024-07-27 06:26:31 -0300

    lógica do ciclo de dia e noite iniciada

--- Assets/Scripts/Event.cs
+++ Assets/Scripts/Event.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Event", menuName = "Events/New Event")]
+[CreateAssetMenu(fileName = "New Event", menuName = "Events/Event")]
 public class Event : ScriptableObject
 {
     public string speakerName;
     public string text;
-    public EventChoice[] choices;
+    public EventChoice[] choices = new EventChoice[2];
 }
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public enum DayMoments
+    {
+        Morning,
+        Evening,
+        Night
+    }
     public float maxFood { get; private set; } = 100;
     public float currentFood { get; private set; }
     public float maxWater { get; private set; } = 100;
@@ -13,8 +19,13 @@ public class GameManager : MonoBehaviour
     public float currentSatisfaction { get; private set; }
     public float maxRemedy { get; private set; } = 100;
     public float currentRemedy { get; private set; }
-    public int currentDay { get; private set; }
+    public int currentDay { get; private set; } = 1;
+
     [SerializeField] Event currentEvent;
+
+    readonly Object[][] events = new Object[3][];
+
+    public DayMoments DayMoment { get; private set; }
 
     private void Awake()
     {
@@ -23,12 +34,21 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        DayMoment = DayMoments.Morning;
+        GetEvents();
         currentFood = 50;
         currentWater = 50;
         currentRemedy = 50;
         currentSatisfaction = 50;
         UIManager.Instance.UpdateSliders();
-        StartEvent();
+        GetNewEvent();
+    }
+
+    private void GetEvents()
+    {
+        events[0] = Resources.LoadAll("Events/Morning");
+        events[1] = Resources.LoadAll("Events/Evening");
+        events[2] = Resources.LoadAll("Events/Night");
     }
 
     public void StartEvent()
@@ -45,5 +65,27 @@ public class GameManager : MonoBehaviour
         currentWater = Mathf.Clamp(currentWater + influence.water, 0, maxWater);
         UIManager.Instance.UpdateSliders();
         UIManager.Instance.HideEvent();
+        NextEvent();
+    }
+
+    private void NextEvent()
+    {
+        if(DayMoment == DayMoments.Night)
+        {
+            DayMoment = DayMoments.Morning;
+            currentDay++;
+        }
+        else
+        {
+            DayMoment++;
+        }
+        GetNewEvent();
+    }
+
+    private void GetNewEvent()
+    {
+        int index = Random.Range(0, events[(int)DayMoment].Length);
+        currentEvent = (Event)events[(int)DayMoment][index];
+        StartEvent();
     }
 }
